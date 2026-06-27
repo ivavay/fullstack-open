b74586fe0e0161c64c44c1a0e4ac119147eafa7d
Ivy Chen <ivy0925chen@gmail.com>
2026-06-27 15:49:48 +0800

    finish anecdote

--- part-1/anecdotes/src/App.jsx
+++ part-1/anecdotes/src/App.jsx
@@ -15,20 +15,35 @@ const App = () => {
   // selected stores the index of the anecdote currently shown.
   // The initial value 0 means the app starts with the first anecdote.
   const [selected, setSelected] = useState(0);
+  const [votes, setVotes] = useState(Array(anecdotes.length).fill(0));
+  const mostVotes = Math.max(...votes);
+  const mostVotedIndex = votes.indexOf(mostVotes);
 
   // Pick a random valid index from the anecdotes array.
   // Math.random() gives a decimal, and Math.floor() rounds it down.
   const randomAnecdote = () => Math.floor(Math.random() * anecdotes.length);
+  const vote = () => {
+    const newVotes = [...votes];
+    newVotes[selected] += 1;
+    setVotes(newVotes);
+  };
 
   return (
     <div>
+      <h1>Anecdote of the day</h1>
       {/* Display the anecdote at the currently selected index. */}
       <div>{anecdotes[selected]}</div>
+      <div>has {votes[selected]} votes</div>
 
+      <button onClick={vote}>vote</button>
       {/* When clicked, choose a new random index and re-render the app. */}
       <button onClick={() => setSelected(randomAnecdote())}>
         next anecdote
       </button>
+
+      <h1>Anecdote with most votes</h1>
+      <div>{anecdotes[mostVotedIndex]}</div>
+      <div>has {mostVotes} votes</div>
     </div>
   );
 };

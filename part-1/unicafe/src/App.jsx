1aa3abb14bd54fa772ca89f64b2ace8d59546108
Ivy Chen <ivy0925chen@gmail.com>
2026-06-27 11:46:33 +0800

    add average and positive score

--- part-1/unicafe/src/App.jsx
+++ part-1/unicafe/src/App.jsx
@@ -5,6 +5,9 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const all = good + neutral + bad
+  const average = all === 0 ? 0 : (good - bad) / all
+  const positive = all === 0 ? 0 : (good / all) * 100
 
   return (
     <div>
@@ -17,6 +20,8 @@ const App = () => {
       <div>good {good}</div>
       <div>neutral {neutral}</div>
       <div>bad {bad}</div>
+      <div>average {average}</div>
+      <div>positive {positive} %</div>
     </div>
   )
 }

9fbfa7c0bd5238fdfc4375ef1c70d2efa1a784d8
Ivy Chen <ivy0925chen@gmail.com>
2026-07-01 17:34:26 +0800

    add put method for changing exsiting persons number to new if duplicate names are added

--- part-3/phonebook-backend/index.js
+++ part-3/phonebook-backend/index.js
@@ -107,6 +107,25 @@ app.get('/api/persons/:id', (request, response) => {
  
 })
 
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+
+  const person = {
+    name: body.name,
+    number: body.number
+  }
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedPerson => {
+      if (updatedPerson) {
+        response.json(updatedPerson)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
+})
+
 // Delete a person by ID (exercise 3.4)
 app.delete('/api/persons/:id', (request, response, next) => {
   Person.findByIdAndDelete(request.params.id)

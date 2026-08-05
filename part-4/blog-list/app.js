1c1863677c59a40055b5ebcdd7ec7c784073ce3a
Ivy Chen <ivavay@Ivys-MacBook-Neo.local>
2026-08-05 16:28:32 +0800

    refactor to course-style structure

---
+++ part-4/blog-list/app.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose')
+const express = require('express')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+const middleware = require('./utils/middleware')
+const blogsRouter = require('./controllers/blogs')
+
+const app = express()
+
+logger.info('Connecting to MongoDB')
+
+mongoose
+  .connect(config.MONGODB_URI, { family: 4 })
+  .then(() => {
+    logger.info('Connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('Error connecting to MongoDB:', error.message)
+  })
+
+app.use(express.json())
+app.use(middleware.requestLogger)
+
+app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
